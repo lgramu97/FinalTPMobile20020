3b4e73b2c95dfbcaa2de4a27d1c93641b2e70d2f
lgramu97 <gramuglialautaro@gmail.com>
2020-03-21 20:15:55 -0300

    Implementacion okHttp

---
+++ app/src/main/java/com/example/tpfinalmoviles/ConfigOkHttp.java
@@ -0,0 +1,63 @@
+package com.example.tpfinalmoviles;
+
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.*;
+import static android.content.ContentValues.TAG;
+
+public class ConfigOkHttp {
+    private OkHttpClient client;
+    public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
+
+    public void POST(String url, JSONObject json){
+        client = new OkHttpClient();
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody body = RequestBody.create(json.toString(), JSON);
+
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .header("Accept", "application/json")
+                .header("Content-Type", "application/json")
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                String mMessage = e.getMessage().toString();
+                Log.w("failure Response", mMessage);
+                //call.cancel();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String mMessage = response.body().string();
+                Log.e(TAG, mMessage);
+            }
+        });
+
+    };
+
+    public void GET(String url){
+        Request request = new Request.Builder().url(url).build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                String mMessage = e.getMessage().toString();
+                Log.w("failure Response", mMessage);
+                //call.cancel();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String mMessage = response.body().string();
+                Log.e(TAG, mMessage);
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/tpfinalmoviles/MainActivity.java
+++ app/src/main/java/com/example/tpfinalmoviles/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.tpfinalmoviles;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView mTextViewResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTextViewResult = findViewById(R.id.text_view_result);
+
+        ConfigOkHttp config = new ConfigOkHttp();
+        JSONObject postdata = new JSONObject();
+        try {
+            postdata.put("username", "aneh1234");
+            postdata.put("password", "12345");
+        } catch(JSONException e){
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        String url = "https://cakeapi.trinitytuts.com/api/add";
+
+        config.POST(url,postdata);
     }
 }
