5a57e484dc6d823a6e3a89c4a1ce2f376f61107f
Juan Stampone <juanstampone@gmail.com>
2020-03-25 15:58:21 -0300

    Calendario listo

--- app/src/main/java/com/example/tpfinalmoviles/AgregarVaca.java
+++ app/src/main/java/com/example/tpfinalmoviles/AgregarVaca.java
@@ -2,10 +2,14 @@ package com.example.tpfinalmoviles;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
@@ -24,19 +28,40 @@ public class AgregarVaca extends AppCompatActivity {
     private Tarea tareaVaca;
     private Button btnAgregarVaca;
     private Button btnRegresarVaca;
-    private TextView cantidadPartos;
+    private EditText etCantidadPartos;
+    private EditText etFechaNacimiento;
+    private EditText etFechaUltParto;
+    private Long idCantPartos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_vaca);
         btnAgregarVaca = (Button) findViewById(R.id.idCargarVaca);
         btnRegresarVaca = (Button) findViewById(R.id.idbBackVaca);
+        etFechaNacimiento = (EditText) findViewById(R.id.idFechaNacimiento);
+        etFechaUltParto = (EditText) findViewById(R.id.idUltFechaParto);
+        etCantidadPartos = (EditText) findViewById(R.id.idCantidadDePartos);
+
+        etFechaUltParto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog(2);
+            }
+        });
+        etFechaNacimiento.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog(1);
+
+            }
+        });
         btnAgregarVaca.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 System.out.println("Dale river");
+                System.out.println("aaas" + etCantidadPartos.getText().toString());
                 tareaVaca = new Tarea();
-                tareaVaca.execute();
+                tareaVaca.execute(etCantidadPartos.getText().toString());
             }
         });
         btnRegresarVaca.setOnClickListener(new View.OnClickListener() {
@@ -47,15 +72,46 @@ public class AgregarVaca extends AppCompatActivity {
         });
 
     }
-    private class Tarea extends AsyncTask<Void,Void,Void>{
+
+    private void showDatePickerDialog(int p ) {
+        if (p == 1 ){
+
+            DatePickerFragment newFragment = DatePickerFragment.newInstance(new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                    // +1 because January is zero
+                    final String selectedDate = day + " / " + (month + 1) + " / " + year;
+                    etFechaNacimiento.setText(selectedDate);
+
+                }
+            });
+
+            newFragment.show(getSupportFragmentManager(), "datePicker");
+        }
+        else{
+            DatePickerFragment nf = DatePickerFragment.newInstance(new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                    // +1 because January is zero
+                    final String selectedDate = day + " / " + (month + 1) + " / " + year;
+                    etFechaUltParto.setText(selectedDate);
+                }
+            });
+
+            nf.show(getSupportFragmentManager(), "datePicker");
+        }
+    }
+
+    private class Tarea extends AsyncTask<String,Void,Void>{
 
         @Override
-        protected Void doInBackground(Void... voids){
+        protected Void doInBackground(String... strings){
+            System.out.println("jejejeje" + strings[0]);
             String url = getSharedPreferences(ConfigServer.URL_DETAILS,MODE_PRIVATE).getString("url","")+"api/cow/";
             ConfigOkHttp peticion = new ConfigOkHttp();
             JSONObject jsonVaca = new JSONObject();
             try {
-                jsonVaca.put("cantidadPartos",0);
+                jsonVaca.put("cantidadPartos",Long.valueOf(strings[0]));
                 jsonVaca.put("electronicId",7);
                 jsonVaca.put("fechaNacimiento","2018-10-09");
                 jsonVaca.put("herdId",1);
